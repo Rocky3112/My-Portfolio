7388d526137cf5f5896db396fda6532a1161d966
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-10-05 15:55:33 +0600

    used react particles

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,94 @@
+import { useCallback } from "react";
 import AboutMe from "../AboutMe/AboutMe";
 import Banner from "../Banner/Banner";
 import Contact from "../Contact/Contact";
 import MySkills from "../MySkills/MySkills";
 // import Project from "../Projects/Project";
 import Projects from "../Projects/Projects";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+
+
 
 const Home = () => {
-    
+      const particlesInit = useCallback(async (engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
     return (
         <div>
+             <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 400,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 1,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.9,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#0d47a1",
+            },
+            links: {
+              color: "#0d47a1",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 3,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 12,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
             <Banner></Banner>
             <AboutMe></AboutMe>
             <MySkills></MySkills>
